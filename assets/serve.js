a0056335ec6e9c4dc7752f5e777dbcf648279144
Marcelo Leonardo <marcelo.leonardo@academico.ifpb.edu.br>
2024-08-02 13:57:21 -0300

    Update serve.js

--- assets/serve.js
+++ assets/serve.js
@@ -1,1 +1,35 @@
+const express = require('express');
+cont app = apress();
+cont PORT = 3000;
 
+app.use(express.json());
+
+// Dados temporários armazenados em um array
+let items = [];
+
+//Rotas POST para adicionar um item
+app.post('/items', (req, res) => {
+  const {name} = req.body;
+  if (!name) {
+    return res.status(400).send("nome do item é necessário') 
+  }                                
+const newItem = { id: items.length + 1, name };
+  items.push(newItem);
+  res.status(201).json(newItem);
+});
+
+//Rotas GET para obter todos os itens
+app.get('/items/:id', (req, res) => {
+  const item = items.find(i => i.id ===parseInt(req.params.id));
+  return res.status(404).send('Item não encontrador');
+});
+
+//Rota DELETE para remover um item por ID
+app.delete('/item/:id', (req, res) => {
+  items = items. filter(i =>i.id !== parseInt( req. params.id));
+  res.status(204).send();
+});
+app.listen(PORT, () => {
+  console.log('servidor rodando na porta ${PORT);
+  }):
+  
